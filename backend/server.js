34e6b5f761959c28fb7eb97c81044bf281368bf4
PRIYANSHI-eng <priyanshi.chittora@kalvium.community>
2025-04-08 14:26:56 +0530

    mongodb setup

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require("cors");
+
 require("dotenv").config();
 
 const app = express();
-app.use(cors());
+
 app.use(express.json());
 
 const PORT = process.env.PORT || 8000;
