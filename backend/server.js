3687d7e91d8ef8a9da4c8b770d771a18d1b6bf6c
PRIYANSHI-eng <priyanshi.chittora@kalvium.community>
2025-04-09 10:51:06 +0530

    get endpoints

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,24 @@
 const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
-const userRoutes = require('./routes/userRoutes');
+
+const userRoutes = require("./routes/userRoutes");
+
 const app = express();
 app.use(express.json());
+
 const PORT = process.env.PORT || 8000;
-app.use('/api/users', userRoutes);
+
+app.use("/api/users", userRoutes);
+
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
-    console.log('MongoDB connected successfully');
+    console.log("MongoDB connected successfully");
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
     });
   })
   .catch((error) => {
-    console.error('MongoDB connection failed:', error.message);
+    console.error("MongoDB connection failed:", error.message);
   });
-  
