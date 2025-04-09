b07da2ff2cd35a4568a0886400a85f27faa3037c
PRIYANSHI-eng <priyanshi.chittora@kalvium.community>
2025-04-09 10:42:22 +0530

    get endpoints

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require("cors");
 require("dotenv").config();
 
+const userRoutes = require('./routes/userRoutes');
+
+
 const app = express();
-app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 8000;
+
+app.use('/api/users', userRoutes);
+
+
 
 mongoose
   .connect(process.env.MONGO_URI)
